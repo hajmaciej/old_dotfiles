4e9d82a230b15636b7cc394caf67fe778f9f6e16
Maciej Haj <pan.efem@gmail.com>
2019-01-16 21:49:12 +0100

    work has to be done ;)

--- INSTALL/vim.sh
+++ INSTALL/vim.sh
@@ -2,6 +2,38 @@
 
 # git clone https://github.com/tmux/tmux.git 
 
-curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+VIMPLUG_DIR="$HOME/.vim/autoload/plug.vim"
+VIMPLUG_GITHUB="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+
+if [[ -s $VIMPLUG_DIR ]]; then
+	echo "VimPlug already installed"
+else
+	curl -fLo $VIMPLUG_DIR --create-dirs $VIMPLUG_GITHUB
+fi
+# https://stackoverflow.com/questions/13522599/how-to-run-vim-commands-from-terminal
+
+# if file is regular file or does not exist it has to be made as a sybolic link to the file in a dotfile folder
+if [[ $(file $HOME/.vimrc) =~ 'text' ]] || [[ ! -e $HOME/.vimrc ]]; then 
+	echo "it does not exist or is a regular"
+	replay=$(source $HOME/git/dotfiles/INSTALL/links -a .vimrc)
+	echo $replay
+	if [[ $replay == 0 ]]; then
+		echo "BYLO ZERO"
+	else
+		echo "NIE BYLO"
+	fi
+# 	if [[ $($HOME/git/dotfiles/INSTALL/links -a .vimrc) != 0 ]]; then
+# 		echo "Some kind of error occured."
+# 		exit 1
+# 	fi
+fi
+
+# if [[ ! $(vim -c 'PlugInstall') ]]; then
+# 	echo "Some kind of error occured."
+# 	exit 2
+# else
+# 	echo "It appears that everything went just fine..."
+# 	exit 0
+# fi
 
 
